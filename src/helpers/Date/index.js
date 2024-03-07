78cfa609f8256282956ed4d3395feb40f24d7132
zidanegh <zidanebekki@outlook.com>
2024-03-07 17:18:04 +0100

    first commit

---
+++ src/helpers/Date/index.js
@@ -0,0 +1,16 @@
+export const MONTHS = {
+  0: "janvier",
+  1: "février",
+  2: "mars",
+  3: "avril",
+  4: "mai",
+  5: "juin",
+  6: "juillet",
+  7: "août",
+  8: "septembre",
+  9: "octobre",
+  10: "novembre",
+  11: "décembre",
+};
+
+export const getMonth = (date) => MONTHS[date.getMonth()];
